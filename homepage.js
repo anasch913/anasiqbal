ca2df0873b5b97b999977cdf64bb5e524360c3da
Anas Iqbal <anasch913@gmail.com>
2026-07-01 20:46:20 +0500

    Added budget simulator link to homepage

--- homepage.js
+++ homepage.js
@@ -1,1 +1,19 @@
 
+
+const button = document.querySelector('.my-button');
+
+button.addEventListener('click', () => {
+    alert('Hello world');
+});
+
+// function myFunction() {
+//   document.querySelector(".my-button").click();
+// }
+
+function colorchange() {
+    document.querySelector(".my-button").style.backgroundColor = "red";
+    document.querySelector(".my-button").style.position = "absolute";
+
+    document.querySelector(".my-button").style.left = "200px";
+    document.querySelector(".my-button").style.top = "200px";
+} 
